cedc9d734776bd3ae8213c308af83bfdb7fc025c
carlosviniciusananias <carlos.oliveira69@fatec.sp.gov.br>
2020-02-13 18:32:11 -0300

    inserindo footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import './footer-style.css'
+
 const Footer = () => {
     return (
-        <div className="footer">
-            <p>footer</p>
+        <div className="footer-section">
+            <div className="footer-about">
+                <div className="about">
+                    <h3>about</h3>
+                </div>
+                <div className="about">
+                    <h3>about</h3>
+                </div>
+                <div className="about">
+                    <h3>about</h3>
+                </div>
+            </div>
         </div>
     );
 };
